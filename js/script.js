a36a0288248d18f969ae4e16fcecdd36821b878f
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-22 11:59:16 +0200

    arrey per le funzioni

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+//prendo le arrey che mi serviranno per le funzioni
+    let numeriCasuali = [];
+    let numeriUtente = [];
+    let numeroCasuale;
+    let numeroInserito;
+    let msg = document.getElementById('msg');
+  
+
+      
+     
+  
+  
+  
+  
+  
+ 
